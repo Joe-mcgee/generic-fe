9a69091616ea3c1f04f8d6f4d94f8d2c62b1cb55
Joe-mcgee <thulsmans.2994@me.com>
2021-01-10 13:31:30 -0700

    moved and improved delete user rail

--- src/components/auth/DeleteUser.vue
+++ src/components/auth/DeleteUser.vue
@@ -1,17 +1,53 @@
 <template>
   <v-container
-    px-0
+    class="pa-3"
     fluid
     fill-height>
-    <v-btn
-      block
-      :disabled="!valid"
-      color="error"
-      class="mr-4"
-      @click="validate"
+    <v-dialog
+      v-model="dialog"
+      width="500"
     >
-      Delete User
-    </v-btn>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          block
+          color="error"
+          v-bind="attrs"
+          v-on="on"
+        >
+           Delete User
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="headline grey lighten-2">
+        Warning, Permanent Operation
+        </v-card-title>
+
+        <v-card-text>
+          Clicking confirm here will delete data associated with your account, one will not be able to login or retreive old information. Continue?
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="error"
+            text
+            @click="validate"
+          >
+            Delete User
+          </v-btn>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            Cancel
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   <v-snackbar
     v-model="deleteSuccess"
     top
@@ -32,15 +68,14 @@
 import authService from '@/services/auth-service.js'
 import { bus } from '@/main.js'
   export default {
-    props: {
-      id: String,
-    },
     data: () => ({
+      userId: '',
+      dialog: false,
       deleteSuccess: false,
       deleteFailure: false,
       valid: true,
     }),
-    created() {
+    async created() {
       bus.$on('deleteSuccess', (event) => {
         this.deleteSuccess = true
         console.log(event)
@@ -48,21 +83,25 @@ import { bus } from '@/main.js'
       bus.$on('deleteFailure', (event) => {
         console.log(event)
         this.deleteFailure = true
-      }) 
+      })
+      let response = await authService.getMe()
+      if (response.success) {
+        this.userId = response.data['_id']
+      }
     },
     methods: {
       async validate () {
           let response
           try {
-            console.log(this.$props.id)
+            console.log(this.userId)
             const data = {
-              id: this.$props.id
+              id: this.userId
             }
             response = await authService.deleteUser(data)
           } catch (e) {
 
             bus.$emit('deleteFailure', {})
-            this.$data.valid = !this.valid
+            this.valid = !this.valid
             console.log(e)
             return
           }
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -2,32 +2,27 @@
   <Card
     title="Update Profile"
     >
-    <v-divider>
-    </v-divider>
     <v-card>
-      <v-card-title
-        >Update Credentials</v-card-title>
+      <v-card-title>Update Credentials</v-card-title>
     </v-card>
-    <v-divider>
-    </v-divider>
     <AuthForm
       v-if="oauth"
       mode="oauth-me"/>
     <AuthForm
       v-else
       mode="me"/>
-    <v-divider>
-    </v-divider>
+
+    <v-divider />
     <v-card>
       <v-card-title
         v-if="!oauth"
         >Update Password</v-card-title>
     </v-card>
-    <v-divider>
-    </v-divider>
     <AuthForm
       v-if="!oauth"
       mode="updatepassword"/>
+    <v-divider />
+    <DeleteUser />
   </Card>
 </template>
 
@@ -35,13 +30,15 @@
 // @ is an alias to /src
 import Card from '@/components/Card.vue'
 import AuthForm from '@/components/auth/AuthForm.vue'
+import DeleteUser from '@/components/auth/DeleteUser.vue'
 import VueCookies from 'vue-cookies'
 
 export default {
   name: 'Home',
   components: {
     AuthForm,
-    Card
+    Card,
+    DeleteUser
   },
   data: () => ({
     oauth: false
